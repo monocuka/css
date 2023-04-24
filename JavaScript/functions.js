0007cb8e4d68c9b371c5b8a8584bfca4fea72261
eliana osorio <yasminosorio2013@gmail.com>
2023-04-24 17:11:51 -0500

    feat: temperature converter

---
+++ JavaScript/functions.js
@@ -0,0 +1,5 @@
+const convertAgeToSeconds = (age) => age * 365 * 24 * 60 * 60;
+
+const temCelsiusToFarenheit = (celsius) => (celsius * 1.8) + 32;
+
+export {convertAgeToSeconds, temCelsiusToFarenheit};
