794d1319d7e1fc961744ccbf2a0f849fb0ac03eb
eliana osorio <yasminosorio2013@gmail.com>
2023-04-25 16:43:38 -0500

    feat: exercise # 4 Initials letter

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -4,7 +4,8 @@ const temCelsiusToFarenheit = (celsius) => (celsius * 1.8) + 32;
 
 const getProfits = (earnings, costs, taxes) => (earnings - costs) * ((100-taxes)/100);
 
-const firstLetter =(FisrtName) => FisrtName.charAt(0);    
+const firstLetter =(FisrtName) => FisrtName.charAt(0).toUpperCase();    
 
 
-export {convertAgeToSeconds, temCelsiusToFarenheit, getProfits, firstLetter};
+
+export {convertAgeToSeconds, temCelsiusToFarenheit, getProfits, firstLetter,};
